de15e8da8e9553a93447b9c526b31132587b48d6
KatyEkat <113355751+KatyEkat@users.noreply.github.com>
2023-10-22 18:47:22 +0300

    hookah proj

---
+++ src/page/index.js
@@ -0,0 +1,46 @@
+// import Swiper from 'swiper';
+
+// const swiper = new Swiper('.mySwiper', {
+// 	effect: 'coverflow',
+// 	grabCursor: true,
+// 	initialSlide: 2,
+// 	centeredSlides: true,
+// 	slidesPerView: '5',
+// 	spaceBetween: 30,
+// 	coverflowEffect: {
+// 		rotate: 0,
+// 		stretch: 0,
+// 		depth: 40,
+// 		modifier: 3,
+// 		slideShadows: false,
+// 	},
+// });
+
+const header = document.querySelector('.header_static');
+window.addEventListener('scroll', function () {
+	if (window.scrollY >= 5) {
+		header.classList.add('header_scrolling');
+	} else {
+		header.classList.remove('header_scrolling');
+	}
+});
+
+// const swiper = new Swiper('.mySwiper', {
+// 	effect: 'coverflow',
+// 	grabCursor: true,
+// 	initialSlide: 2,
+// 	centeredSlides: true,
+// 	slidesPerView: '5',
+// 	spaceBetween: 30,
+// 	coverflowEffect: {
+// 		rotate: 0,
+// 		stretch: 0,
+// 		depth: 40,
+// 		modifier: 3,
+// 		slideShadows: false,
+// 	},
+// 	navigation: {
+// 		nextEl: '.swiper_btn_next',
+// 		prevEl: '.swiper_btn_prev',
+// 	},
+// });
